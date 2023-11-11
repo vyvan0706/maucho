8a85723e59be0cb9061306486f94ba32c117a26b
vyvan0706 <150523177+vyvan0706@users.noreply.github.com>
2023-11-11 17:06:22 +0000

    vyvan07070

--- Hello.py
+++ Hello.py
@@ -2,7 +2,7 @@ import streamlit as st
 import sys
 print("Python Version:", sys.version)
 print("Python Path:", sys.executable)
-from gtts import gTTS
+# from gtts import gTTS
 from ftlangdetect import detect as lang_detector
 from deep_translator import GoogleTranslator
 
