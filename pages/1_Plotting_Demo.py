211e09b247c88ab36e442649d56f5c12c985dd1f
vyvan0706 <150523177+vyvan0706@users.noreply.github.com>
2023-11-12 05:22:04 +0000

    conmeokeugaugau

--- pages/1_Plotting_Demo.py
+++ pages/1_Plotting_Demo.py
@@ -1,56 +1,14 @@
-# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-import time
-
-import numpy as np
-
+# YOUR CODE
 import streamlit as st
-from streamlit.hello.utils import show_code
-
-
-def plotting_demo():
-    progress_bar = st.sidebar.progress(0)
-    status_text = st.sidebar.empty()
-    last_rows = np.random.randn(1, 1)
-    chart = st.line_chart(last_rows)
-
-    for i in range(1, 101):
-        new_rows = last_rows[-1, :] + np.random.randn(5, 1).cumsum(axis=0)
-        status_text.text("%i%% Complete" % i)
-        chart.add_rows(new_rows)
-        progress_bar.progress(i)
-        last_rows = new_rows
-        time.sleep(0.05)
-
-    progress_bar.empty()
-
-    # Streamlit widgets automatically run the script from top to bottom. Since
-    # this button is not connected to any other logic, it just causes a plain
-    # rerun.
-    st.button("Re-run")
-
-
-st.set_page_config(page_title="Plotting Demo", page_icon="📈")
-st.markdown("# Plotting Demo")
-st.sidebar.header("Plotting Demo")
-st.write(
-    """This demo illustrates a combination of plotting and animation with
-Streamlit. We're generating a bunch of random numbers in a loop for around
-5 seconds. Enjoy!"""
-)
-
-plotting_demo()
-
-show_code(plotting_demo)
+tab1,tab2,tab3=st.tabs(['Trang chủ','Cà phê','Trà'])
+with tab1:
+  st.image('https://hcm.fstorage.vn/images/2023/11/banner-canvas-20231108021333.jpg')
+  col1,col2=st.columns(2)
+  with col1:
+    st.image('https://phuclong.com.vn/uploads/post/20649d183ca5f1-bannertrangchu.jpg')
+  with col2:
+    st.text('Từ những mầm trà, chúng tôi tạo ra niềm đam m ê')
+with tab2:
+  st.image("https://phuclong.com.vn/upload/files/2/tra%20cafe/Dam%20vi%20ca%20phe%203''.jpg")
+  st.header('HẠT CÀ PHÊ PHÚC LONG')
+  tab21,tab22,tab23,tab24=st.tabs(['Đậm vị hạt cà phê rang xay','Hương vị tinh tế','Hương vị mạnh mẽ','Cà phê mùi'])
