8ff13da17d4f9928564b20285ea87d7106117eb4
vyvan0706 <150523177+vyvan0706@users.noreply.github.com>
2023-11-12 06:34:37 +0000

    dmcasmdlasdf

--- pages/0_Animation_Demo.py
+++ pages/0_Animation_Demo.py
@@ -1,13 +1,24 @@
 import streamlit as st
-import numpy as np
 
 message = st.chat_message("A")
-message.write("Hello human:alien:")
-namelist=['thư','anh','hương','tú','thanh','vy','phương']
-prompt = st.chat_input("Nhập tên")
-if prompt in namelist
-    st.write(f"Chào bạn cuti: {prompt}")
-else: 
-    st.write(f"Bạn là ai z huheo")
-    
-    
+message.write("Hello human :alien:")
+
+namelist = ['thư', 'anh', 'hương', 'tú', 'thanh', 'vy', 'phương','uyên','cindy', 'mary', 'tatyana', 'katy', 'alina', 'ivy', 'julie','elena']
+
+prompt = st.chat_input("Nhập tên", key='name')
+
+if prompt is not None:
+    prompt_lower = prompt.lower()
+    if prompt_lower in namelist:
+        message = st.chat_message("A")
+        message.write(f"TRỜI ƠI HÍ LU {prompt.upper()} ")
+        message = st.chat_message("A")
+        message.write('MÌNH CHU CÀ MO BẠN')
+    else:
+        message = st.chat_message("A")
+        message.write("Bạn là ai z huheo")
+        message = st.chat_message("A")
+        message.write(f"Chào bạn {prompt} cuti nho")
+else:
+    message = st.chat_message("A")
+    message.write("Please enter a name.")
